a41ecbf10b2cf08e38da8e8c928c8855614d0cbc
ss77995ss <ss77995ss@gmail.com>
2020-11-23 23:37:24 +0800

    feat(Home): Create main regions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import { ChakraProvider, Box, Flex, Heading } from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">This is header</header>
-      <section className="App-body">This is body</section>
-    </div>
+    <ChakraProvider>
+      <Box height="100vh" className="App">
+        <Box as="header" className="App-header" boxShadow="rgba(0, 0, 0, 0.05) 0px 1px 2px 0px">
+          <Heading m={0} p={2}>
+            Youtube Manager
+          </Heading>
+        </Box>
+        <Flex height="100%" as="section" className="App-body">
+          <Box width={280} height="100%" borderRight="1px solid #ccc" p={3} as="aside">
+            This is sidebar
+          </Box>
+          <Box width="100%" height="100%" p={3} textAlign="center" as="section">
+            This is body
+          </Box>
+        </Flex>
+      </Box>
+    </ChakraProvider>
   );
 }
 
