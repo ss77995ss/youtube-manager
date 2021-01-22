8de47536fad263d62456106b92f9d0ae37c2a7fe
ss77995ss <ss77995ss@gmail.com>
2021-01-22 23:34:12 +0800

    feat: Groupby sidebar videos by categories

--- src/Sidebar/index.js
+++ src/Sidebar/index.js
@@ -1,14 +1,9 @@
-import { useState } from 'react';
 import { Box, Input, Text, List, ListItem } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
-import { useVideosCtx } from '../hooks/useVideos';
+import useSidebar from '../hooks/useSidebar';
 
 const Sidebar = () => {
-  const { videos } = useVideosCtx();
-  const [searchKeyword, setSearchKeyword] = useState('');
-  const resolvedVideos = videos.filter(({ title }) => title.toLowerCase().includes(searchKeyword.toLowerCase()));
-
-  const handleKeywordChange = (event) => setSearchKeyword(event.target.value);
+  const { searchType, searchKeyword, groupByVideos, lastCategoriesKey, handleKeywordChange } = useSidebar();
 
   return (
     <Box
@@ -20,18 +15,48 @@ const Sidebar = () => {
       as="aside"
     >
       <Input mb={2} value={searchKeyword} placeholder="搜尋影片" onChange={handleKeywordChange} />
-      {resolvedVideos.length > 0 ? (
+      {searchType === 'categories' && Object.keys(groupByVideos).length > 0 && (
         <List>
-          {resolvedVideos.map((video, index) => (
-            <Link to={`/show/${video.id}`}>
-              <ListItem key={`sidebar-item-${index}`} cursor="pointer" _hover={{ bg: 'gray.300' }}>
-                {video.title}
-              </ListItem>
-            </Link>
-          ))}
+          {Object.keys(groupByVideos).map((key, index) =>
+            key === lastCategoriesKey ? (
+              <>
+                <Text key={`category-${key}-${index}`} fontWeight={500}>
+                  無類別
+                </Text>
+                {groupByVideos[key].length > 0 &&
+                  groupByVideos[key].map((video, index) => (
+                    <Link to={`/show/${video.id}`}>
+                      <ListItem
+                        key={`sidebar-category-${key}-item-${index}`}
+                        cursor="pointer"
+                        _hover={{ bg: 'gray.300' }}
+                      >
+                        {video.title}
+                      </ListItem>
+                    </Link>
+                  ))}
+              </>
+            ) : (
+              <>
+                <Text key={`category-${key}-${index}`} fontWeight={500}>
+                  {key}
+                </Text>
+                {groupByVideos[key].length > 0 &&
+                  groupByVideos[key].map((video, index) => (
+                    <Link to={`/show/${video.id}`}>
+                      <ListItem
+                        key={`sidebar-category-${key}-item-${index}`}
+                        cursor="pointer"
+                        _hover={{ bg: 'gray.300' }}
+                      >
+                        {video.title}
+                      </ListItem>
+                    </Link>
+                  ))}
+              </>
+            ),
+          )}
         </List>
-      ) : (
-        <Text>查無影片</Text>
       )}
     </Box>
   );
